26fd6e401f27abda4ac5d10d9543966f5646b278
Måns Isaksson <mans.95.isaksson@gmail.com>
2016-03-30 21:02:35 +0200

    Code Cleanup
    Ränsa koden från unödiga saker

---
+++ Two31/Source/Two31/DefaultGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "DefaultGameMode.generated.h"
+
+
+UCLASS()
+class TWO31_API ADefaultGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ADefaultGameMode();
+};
