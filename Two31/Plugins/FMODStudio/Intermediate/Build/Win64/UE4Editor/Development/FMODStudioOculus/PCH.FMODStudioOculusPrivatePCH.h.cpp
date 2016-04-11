f4fd8f3bb664a18f9ee3519ec0b00648540d9fc8
Dwarfley <a14nikjo@hs.local>
2016-04-11 14:36:07 +0200

    Fixed FMOD git ignore 1

--- Two31/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudioOculus/PCH.FMODStudioOculusPrivatePCH.h.cpp
+++ Two31/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudioOculus/PCH.FMODStudioOculusPrivatePCH.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\mans_\Documents\GitHub\Two31\Two31\Plugins\FMODStudio\Source\FMODStudioOculus\Private\FMODStudioOculusPrivatePCH.h"
+#include "C:\Two31\Two31\Two31\Plugins\FMODStudio\Source\FMODStudioOculus\Private\FMODStudioOculusPrivatePCH.h"
