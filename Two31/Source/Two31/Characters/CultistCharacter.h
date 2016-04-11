58fad65cde57fede23ec49e2785b1266c1b5a094
Måns Isaksson <mans.95.isaksson@gmail.com>
2016-04-11 15:30:53 +0200

    Cultist Character Class

---
+++ Two31/Source/Two31/Characters/CultistCharacter.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Characters/EnemyCharacter.h"
+#include "CultistCharacter.generated.h"
+
+
+UCLASS()
+class TWO31_API ACultistCharacter : public AEnemyCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	ACultistCharacter();
+	
+};
