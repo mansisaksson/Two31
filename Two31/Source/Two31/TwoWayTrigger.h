eaf4fa238281bcee93b0549ae62f8aa83927a1b9
ATKelman <a14aleke@hs.local>
2016-04-29 16:52:58 +0200

    Two Way Triggers

---
+++ Two31/Source/Two31/TwoWayTrigger.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "TriggerManager.h"
+#include "TwoWayTrigger.generated.h"
+
+
+UCLASS()
+class TWO31_API ATwoWayTrigger : public ATriggerManager
+{
+	GENERATED_BODY()
+	
+public:
+	ATwoWayTrigger();
+	
+};
