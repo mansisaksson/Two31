d94f76367a1a7190d5b21664f2ca8d62c0b907cf
Måns Isaksson <mans.95.isaksson@gmail.com>
2016-05-12 22:56:40 +0200

    GitIgnore

--- Two31/Plugins/Substance/Intermediate/Build/Win64/UE4Editor/Development/SubstanceEditor/SubstanceEditorPrivatePCH.h
+++ Two31/Plugins/Substance/Intermediate/Build/Win64/UE4Editor/Development/SubstanceEditor/SubstanceEditorPrivatePCH.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_SUBSTANCEEDITORPRIVATEPCH_H__
 #define __AUTO_SUBSTANCEEDITORPRIVATEPCH_H__
-//Last Write: 2016-05-12 20:00:03 +00:00
+//Last Write: 2016-05-12 20:50:34 +00:00
 #include "C:\Users\mans_\Documents\GitHub\Two31\Two31\Plugins\Substance\Source\SubstanceEditor\Private\SubstanceEditorPrivatePCH.h"
 #endif//__AUTO_SUBSTANCEEDITORPRIVATEPCH_H__
